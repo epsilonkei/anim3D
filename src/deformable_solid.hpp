1b68d0720bcf1e8ae4a069bdbdd5bcf01600f103
Khanh Nguyen <nguyen@jsk.imi.i.u-tokyo.ac.jp>
2018-06-17 02:01:05 +0900

    Add deformable solid collision demo for task 3 TODO: remove vibration, add rotation

---
+++ src/deformable_solid.hpp
@@ -0,0 +1,64 @@
+#include <boost/shared_ptr.hpp>
+#include "particle.hpp"
+#include <vector>
+#include <Eigen/Dense>
+#include <algorithm>
+#include <iterator>
+
+extern double grav;
+extern Eigen::Vector3d e1, e2, e3;
+
+#define K_SPRING 1e2
+#define K_DAMPER 1e1
+#define SPRING_DAMPER_THRE 1e-3
+
+class deformable_solid {
+public:
+  std::vector<boost::shared_ptr<particle> > pl;
+  std::vector<std::vector<int> > next_part;
+  double length;
+  Eigen::Vector3d tmp_d;
+
+  deformable_solid() {}
+  ~ deformable_solid() {}
+
+  void add_particle(double _mass, double _radius, double _last_dt, double* _prev_pos, double* _pos, double* _vel, double* _acc) {
+    this->pl.push_back(boost::shared_ptr<particle>(new particle(_mass, _radius, _last_dt, _prev_pos, _pos, _vel, _acc)));
+  }
+
+  void init() {
+    this->length = (this->pl[1]->pos - this->pl[0]->pos).norm();
+    // Create next_particle list, Note: assuming object as a cube
+    std::vector<int> npl;
+    npl.push_back(1); npl.push_back(3); npl.push_back(4);
+    for (int i=0; i<this->pl.size(); i++) {
+      if (i==0) { npl[0] = 1; npl[1] = 3; npl[2] = 4; }
+      else if (i==1) { npl[0] = 0; npl[1] = 2; npl[2] = 5; }
+      else if (i==2) { npl[0] = 1; npl[1] = 3; npl[2] = 6; }
+      else if (i==3) { npl[0] = 0; npl[1] = 2; npl[2] = 7; }
+      else if (i==4) { npl[0] = 0; npl[1] = 5; npl[2] = 7; }
+      else if (i==5) { npl[0] = 1; npl[1] = 4; npl[2] = 6; }
+      else if (i==6) { npl[0] = 2; npl[1] = 5; npl[2] = 7; }
+      else if (i==7) { npl[0] = 3; npl[1] = 4; npl[2] = 6; }
+      this->next_part.push_back(npl);
+    }
+  }
+
+  void update_spring_damper() {
+    // only checked distance with next particles
+    for (int i=0; i<this->pl.size(); i++) {
+      // Add spring and dampers force
+      for (int j=0; j<this->next_part[i].size(); j++) {
+        int nid = this->next_part[i][j];
+        this->tmp_d = (this->pl[nid]->pos - this->pl[i]->pos);
+        double dist = this->tmp_d.norm();
+        if (dist > SPRING_DAMPER_THRE) {
+          this->tmp_d /= dist;
+          this->pl[i]->force += K_SPRING * (dist - this->length) * this->tmp_d;
+          this->pl[i]->force += K_DAMPER * (this->pl[nid]->vel.dot(this->tmp_d)
+                                            - this->pl[i]->vel.dot(this->tmp_d)) * this->tmp_d;
+        }
+      }
+    }
+  }
+};
---
+++ src/floor_deformable_solid.hpp
@@ -0,0 +1,61 @@
+#include "deformable_solid.hpp"
+#include "floor.hpp"
+
+extern double grav;
+extern Eigen::Vector3d e1, e2, e3;
+
+#define WALL_DGAIN 3e3
+#define WALL_VGAIN 5e1
+
+class floor_rigids {
+public:
+  std::vector<boost::shared_ptr<deformable_solid> > rl;
+  std::vector<boost::shared_ptr<static_floor> > floors;
+
+  floor_rigids() {}
+  ~ floor_rigids() {}
+
+  void clearAll(){ this->rl.clear(); this->floors.clear(); }
+
+  void add_floor(double* _org, double* _norm, double _elas) {
+    this->floors.push_back(boost::shared_ptr<static_floor>(new static_floor(_org, _norm, _elas)));
+  }
+
+  void add_particle(double _elas, double _mass, double _radius, double* _prev_pos, double* _pos, double* _vel, double* _acc, int i=0) {
+    while ( this->rl.size() <= i ) this->rl.push_back(boost::shared_ptr<deformable_solid>(new deformable_solid));
+    this->rl[i]->add_particle(_elas, _mass, _radius, _prev_pos, _pos, _vel, _acc);
+  }
+
+
+  void floor_collision_penalty(boost::shared_ptr<particle>& part, bool apply_grav=true) {
+    if (apply_grav) part->force = -part->mass * grav * e3;
+    else part->force = Eigen::Vector3d::Zero();
+    for (int k=0; k<this->floors.size(); k++) {
+      double dist_to_floor = this->floors[k]->norm_vec.dot(part->pos - this->floors[k]->origin)
+        - part->radius;
+      if (dist_to_floor < 0) {
+        part->force += (- WALL_DGAIN*dist_to_floor - WALL_VGAIN*
+                        part->vel.dot(this->floors[k]->norm_vec)) * this->floors[k]->norm_vec;
+      }
+    }
+  }
+
+  void floor_collision_penalty_all(bool apply_grav=true) {
+    for (int i=0; i<this->rl.size(); i++) {
+      for (int j=0; j<this->rl[i]->pl.size(); j++) {
+        floor_collision_penalty(this->rl[i]->pl[j] ,apply_grav);
+      }
+    }
+  }
+
+  void update_movement(double dt) {
+    // Notes: initial force was apply in floor collision penalty function
+    floor_collision_penalty_all(true);
+    for (int i=0; i<this->rl.size(); i++) {
+      this->rl[i]->update_spring_damper();
+      for (int j=0; j<this->rl[i]->pl.size(); j++) {
+        this->rl[i]->pl[j]->updateVerlet(dt);
+      }
+    }
+  }
+};

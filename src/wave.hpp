f4e76042cf2dc2dc74753ef62c4da7f1d2d7224d
Khanh Nguyen <nguyen@jsk.imi.i.u-tokyo.ac.jp>
2018-06-18 01:08:04 +0900

    Add wave demo for task 4 (still need some fix for boundary) TODO: add volume conservation

---
+++ src/wave.hpp
@@ -0,0 +1,146 @@
+#include <iostream>
+#include <Eigen/Dense>
+#include <ctime>
+
+double grav = 9.8;
+
+class wave {
+public:
+  double max_x, max_y;
+  double dt, dx, dy;
+  Eigen::MatrixXd abs_h, gr, h, h_buf;
+  Eigen::MatrixXd vx, vy, vx_buf, vy_buf;
+  //
+  int max_i, max_j;
+  double volume;
+  double ggain;
+
+  wave() {}
+  ~wave() {}
+
+  void init(double _max_x, double _max_y, double _dt, double _dx, double _dy) {
+    this->max_x = _max_x;
+    this->max_y = _max_y;
+    this->dt = _dt;
+    this->dx = _dx;
+    this->dy = _dy;
+    this->max_i = (int)(this->max_x / this->dx);
+    this->max_j = (int)(this->max_y / this->dy);
+    this->abs_h = Eigen::MatrixXd(this->max_i, this->max_j);
+    this->gr = Eigen::MatrixXd::Zero(this->max_i, this->max_j); // set all ground as 0
+    this->h = Eigen::MatrixXd(this->max_i, this->max_j);
+    this->vx = Eigen::MatrixXd(this->max_i, this->max_j);
+    this->vy = Eigen::MatrixXd(this->max_i, this->max_j);
+    this->h_buf = Eigen::MatrixXd(this->max_i, this->max_j);
+    this->vx_buf = Eigen::MatrixXd(this->max_i, this->max_j);
+    this->vy_buf = Eigen::MatrixXd(this->max_i, this->max_j);
+  }
+
+  void set_const_height(double h, int i0, int j0, int di, int dj) {
+    for ( int i=i0; i<di+i0; i++ ) {
+      if ( i>=this->max_i ) break;
+      for ( int j=j0; j<dj+j0; j++ ) {
+        if ( j>=this->max_j ) break;
+        this->h(i,j) = h;
+      }
+    }
+    this->abs_h = this->h + this->gr;
+  }
+
+  void set_random_height(double _h, bool seed=false) {
+    if ( seed ) std::srand(std::time(0));
+    for ( int i=1; i<this->max_i-1; i++ ) {
+      for ( int j=1; j<this->max_j-1; j++ ) {
+        this->h(i,j) += (_h * ((2.0 * std::rand())/RAND_MAX - 1));
+      }
+    }
+    this->abs_h = this->h + this->gr;
+  }
+
+  double get_value_with_bound_check(int i, int j, Eigen::MatrixXd &map) {
+    if ( i < 0 ) i=0; else if(i>=map.rows()) i = map.rows()-1;
+    if ( j < 0 ) j=0; else if(j>=map.cols()) j = map.cols()-1;
+    return map(i,j);
+  }
+
+  double bilinear_interpolate(double x, double y, Eigen::MatrixXd *s) {
+    double i = x / this->dx;
+    double j = y / this->dy;
+    int i0 = (int)i;
+    int j0 = (int)j;
+    int i1 = i0+1;
+    int j1 = j0+1;
+    double w00 = (1 - (i-i0)) * (1 - (j-j0));
+    double w01 = (1 - (i-i0)) * (1 - (j1-j));
+    double w11 = (1 - (i1-i)) * (1 - (j1-j));
+    double w10 = (1 - (i1-i)) * (1 - (j-j0));
+    double ret = this->get_value_with_bound_check(i0,j0,*s) * w00
+      + this->get_value_with_bound_check(i0,j1,*s) * w01
+      + this->get_value_with_bound_check(i1,j1,*s) * w11
+      + this->get_value_with_bound_check(i1,j0,*s) * w10;
+    return ret;
+  }
+
+  void advect(Eigen::MatrixXd *ret, Eigen::MatrixXd *s, Eigen::MatrixXd *v1, Eigen::MatrixXd *v2) {
+    for ( int i=1; i<this->max_i-1; i++ ) {
+      for ( int j=1; j<this->max_j-1; j++ ) {
+        double x_vxij = i*this->dx - this->dt * (*v1)(i,j);
+        double y_vyij = j*this->dy - this->dt * (*v2)(i,j);
+        (*ret)(i,j) = this->bilinear_interpolate(x_vxij, y_vyij, s);
+      }
+    }
+  }
+
+  void update_height() {
+    for ( int i=1; i<this->max_i-1; i++ ) {
+      for ( int j=1; j<this->max_j-1; j++ ) {
+        this->h(i,j) -= this->h(i,j) * ((this->vx(i+1, j) - this->vx(i,j))/this->dx +
+                                        (this->vy(i, j+1) - this->vy(i,j))/this->dy) * this->dt;
+      }
+    }
+  }
+
+  void update_velocity() {
+    for ( int i=1; i<this->max_i-1; i++ ) {
+      for ( int j=1; j<this->max_j-1; j++ ) {
+        this->vx(i,j) += grav * (this->abs_h(i-1,j) - this->abs_h(i,j)) / this->dx * this->dt;
+        this->vy(i,j) += grav * (this->abs_h(i,j-1) - this->abs_h(i,j)) / this->dy * this->dt;
+      }
+    }
+  }
+
+  void constrained_with_reflecting_boundary() {
+    for ( int j=0; j<this->max_j; j++ ) {
+      this->abs_h(0,j) = this->abs_h(1,j);
+      this->vx(1,j) = this->vx(0,j) = this->vy(0,j) = 0;
+      //
+      this->abs_h(this->max_i-1,j) = this->abs_h(this->max_i-2,j);
+      this->vx(this->max_i-2,j) = this->vx(this->max_i-1,j) = this->vy(this->max_i-1,j) = 0;
+    }
+    //
+    for ( int i=0; i<this->max_i; i++ ) {
+      this->abs_h(i,0) = this->abs_h(i,1);
+      this->vx(i,0) = this->vy(i,0) = this->vy(i,1) = 0;
+      //
+      this->abs_h(i,this->max_j-1) = this->abs_h(i,this->max_j-2);
+      this->vx(i,this->max_j-1) = this->vy(i,this->max_j-1) = this->vy(i,this->max_j-2) = 0;
+    }
+  }
+
+  void update() {
+    this->h_buf = this->h; this->vx_buf = this->vx; this->vy_buf = this->vy;
+    // h = advect(h,v)
+    advect(&this->h, &this->h_buf, &this->vx_buf, &this->vy_buf);
+    // vx = advect(vx,v), vy = advect(vy,v)
+    advect(&this->vx, &this->vx_buf, &this->vx_buf, &this->vy_buf);
+    advect(&this->vy, &this->vy_buf, &this->vx_buf, &this->vy_buf);
+    // Update-height
+    update_height();
+    // abs_h = h + gr
+    this->abs_h = this->h + this->gr;
+    // Update-velocity
+    update_velocity();
+    // boundaries condition
+    constrained_with_reflecting_boundary();
+  }
+};
---
+++ src/wave_demo.cpp
@@ -0,0 +1,248 @@
+#include <GL/glut.h>  // GLUT, include glu.h and gl.h
+#include <cmath>
+#include <Eigen/Dense>
+#include <iostream>
+#include "wave.hpp"
+#define PI 3.14159265
+
+extern double grav;
+extern Eigen::Vector3d e1, e2, e3;
+
+#define POOL_MAX_X 2.0
+#define POOL_MAX_Y 2.0
+
+// Global variables
+char title[] = "Full-Screen & Windowed Mode";  // Windowed mode's title
+int windowWidth  = 640;     // Windowed mode's width
+int windowHeight = 480;     // Windowed mode's height
+int windowPosX   = 50;      // Windowed mode's top-left corner x
+int windowPosY   = 50;      // Windowed mode's top-left corner y
+
+int refreshMillis = 30;      // Refresh period in milliseconds
+double dt = 1e-2;
+//double dt = refreshMillis * 1e-3;
+double dx = 0.05, dy = 0.05;
+
+wave WV;
+
+static double org_dist = 5.0, org_pitch = 20.0, org_yaw = 0.0;
+double distance = org_dist, pitch = org_pitch, yaw = org_yaw;
+int mouse_button = -1;
+int mouse_x = 0, mouse_y = 0;
+
+static const GLfloat light_position[] = { 5.0, 5.0, 10.0, 1.0 };
+static const GLfloat light_ambient[] = {1.0, 1.0, 1.0, 1.0};
+static const GLfloat light_diffuse[] = {0.5, 0.5, 0.5, 1.0};
+
+// Projection clipping area
+bool fullScreenMode = false; // Full-screen or windowed mode?
+
+// Color for objects
+float red[] = {0.9, 0.1, 0.1, 1.0};
+float green[] = {0.1, 0.9, 0.1, 1.0};
+float blue[] = {0.1, 0.1, 0.9, 1.0};
+
+/* Initialize OpenGL Graphics */
+void initGL() {
+  // glClearColor (1.0, 1.0, 1.0, 1.0);
+  glClearColor (0.0, 0.0, 0.0, 1.0);
+  glClearDepth( 1.0 );
+
+  // Depth Test
+  glEnable( GL_DEPTH_TEST );
+  glDepthFunc( GL_LESS );
+
+  glShadeModel (GL_SMOOTH);
+
+  // Default light
+  glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+  glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambient);
+  glLightfv(GL_LIGHT0, GL_DIFFUSE, light_ambient);
+  glEnable(GL_LIGHTING);
+  glEnable(GL_LIGHT0);
+}
+
+void initSim() {
+   WV.init(POOL_MAX_X, POOL_MAX_Y, dt, dx, dy);
+   // WV.set_const_height(2, 0, 0, WV.max_i, WV.max_j);
+   WV.set_random_height(2);
+   // std::cerr << WV.max_i << ", " << WV.max_j << std::endl;
+}
+
+void physics_calculate(){
+   WV.update();
+}
+
+void draw_floor(){
+   double l = 10, s = 50;
+   glDisable(GL_LIGHTING); glBegin(GL_LINES);
+   glColor3f(1, 1, 1);
+   for (int y = -s; y <= s; y += l) {
+      for (int x = -s; x <= s; x += l) {
+         glVertex3f (x, -s, 0);
+         glVertex3f (x,  s, 0);
+         glVertex3f (-s, y, 0);
+         glVertex3f ( s, y, 0);
+      }
+   }
+   glEnd(); glEnable(GL_LIGHTING);
+}
+
+void draw_origin(){
+   double l = 1.5;
+   glDisable(GL_LIGHTING); glBegin(GL_LINES);
+   glColor3f(1, 0, 0); glVertex3f(0, 0, 0); glVertex3f(l, 0, 0);
+   glColor3f(0, 1, 0); glVertex3f(0, 0, 0); glVertex3f(0, l, 0);
+   glColor3f(0, 0, 1); glVertex3f(0, 0, 0); glVertex3f(0, 0, l);
+   glEnd(); glEnable(GL_LIGHTING);
+}
+
+void draw_wave() {
+   glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, blue);
+   //
+   for (int i=0; i<WV.max_i; i++) {
+      for (int j=0; j<WV.max_j; j++) {
+         glPushMatrix();
+         glTranslatef (i*WV.dx - 0.5*POOL_MAX_X, j*WV.dy - 0.5*POOL_MAX_Y, WV.abs_h(i,j));
+         // glScalef (WV.dx, WV.dy, 0.5*WV.abs_h(i,j));
+         glutSolidCube(WV.dx);
+         glPopMatrix();
+      }
+   }
+}
+
+/* Callback handler for window re-paint event */
+void display() {
+   glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // Clear the color buffer
+   glMatrixMode(GL_MODELVIEW);    // To operate on the model-view matrix
+   glLoadIdentity ();             /* clear the matrix */
+   /* viewing transformation  */
+   gluLookAt(0.0, 0.0, distance, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+   glRotatef( -pitch, 1.0, 0.0, 0.0 );
+   glRotatef( -yaw, 0.0, 1.0, 0.0 );
+   // draw_floor();
+   draw_origin();
+   draw_wave();
+   glutSwapBuffers();  // Swap front and back buffers (of double buffered mode)
+   physics_calculate();
+}
+
+
+/* Call back when the windows is re-sized */
+void reshape(GLsizei width, GLsizei height) {
+   // Compute aspect ratio of the new window
+   if (height == 0) height = 1;                // To prevent divide by 0
+   GLfloat aspect = (GLfloat)width / (GLfloat)height;
+
+   // Set the viewport to cover the new window
+   glViewport(0, 0, width, height);
+
+   // Set the aspect ratio of the clipping area to match the viewport
+   glMatrixMode(GL_PROJECTION);  // To operate on the Projection matrix
+   glLoadIdentity();             // Reset the projection matrix
+   gluPerspective(60.0, aspect, 1.0, 60.0);
+}
+
+/* Called back when the timer expired */
+void Timer(int value) {
+   glutPostRedisplay();    // Post a paint request to activate display()
+   glutTimerFunc(refreshMillis, Timer, 0); // subsequent timer call at milliseconds
+}
+
+/* Callback handler for normal-key event */
+void keyboard(unsigned char key, int x, int y) {
+   switch (key) {
+   case 27:     // ESC key
+      exit(0);
+      break;
+   case 'r':    // r: Reset default camera
+      distance = org_dist, pitch = org_pitch, yaw = org_yaw;
+      break;
+   }
+}
+
+/* Callback handler for special-key event */
+void specialKeys(int key, int x, int y) {
+   switch (key) {
+   case GLUT_KEY_F1:    // F1: Toggle between full-screen and windowed mode
+      fullScreenMode = !fullScreenMode;         // Toggle state
+      if (fullScreenMode) {                     // Full-screen mode
+         windowPosX   = glutGet(GLUT_WINDOW_X); // Save parameters for restoring later
+         windowPosY   = glutGet(GLUT_WINDOW_Y);
+         windowWidth  = glutGet(GLUT_WINDOW_WIDTH);
+         windowHeight = glutGet(GLUT_WINDOW_HEIGHT);
+         glutFullScreen();                      // Switch into full screen
+      } else {                                         // Windowed mode
+         glutReshapeWindow(windowWidth, windowHeight); // Switch into windowed mode
+         glutPositionWindow(windowPosX, windowPosX);   // Position top-left corner
+      }
+      break;
+   }
+}
+
+/* Callback handler for mouse event */
+void mouse(int button, int state, int x, int y)
+{
+  mouse_button = button;
+  mouse_x = x; mouse_y = y;
+  if(state == GLUT_UP){
+    mouse_button = -1;
+  }
+  glutPostRedisplay();
+}
+
+void motion(int x, int y)
+{
+  switch(mouse_button){
+  case GLUT_LEFT_BUTTON:
+    if( x == mouse_x && y == mouse_y )
+      return;
+    yaw -= (GLfloat) (x - mouse_x) / 2.0;
+    pitch -= (GLfloat) (y - mouse_y) / 2.0;
+    break;
+  case GLUT_RIGHT_BUTTON:
+    if( y == mouse_y )
+      return;
+    if( y < mouse_y )
+      distance += (GLfloat) (mouse_y - y)/50.0;
+    else
+      distance -= (GLfloat) (y - mouse_y)/50.0;
+    if( distance < 1.0 ) distance = 1.0;
+    if( distance > 50.0 ) distance = 50.0;
+    break;
+  }
+  mouse_x = x;
+  mouse_y = y;
+  glutPostRedisplay();
+}
+
+void idle()
+{
+  glutPostRedisplay();
+}
+
+/* Main function: GLUT runs as a console application starting at main() */
+int main(int argc, char** argv) {
+   glutInit(&argc, argv);            // Initialize GLUT
+   glutInitDisplayMode( GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH ); // Enable double buffered mode
+   glutInitWindowSize(windowWidth, windowHeight);  // Initial window width and height
+   glutInitWindowPosition(windowPosX, windowPosY); // Initial window top-left corner (x, y)
+   glutCreateWindow(title);      // Create window with given title
+   glutDisplayFunc(display);     // Register callback handler for window re-paint
+   glutReshapeFunc(reshape);     // Register callback handler for window re-shape
+   glutTimerFunc(0, Timer, 0);   // First timer call immediately
+   glutSpecialFunc(specialKeys); // Register callback handler for special-key event
+   glutKeyboardFunc(keyboard);   // Register callback handler for special-key event
+   glutIdleFunc(idle);
+   glutMouseFunc(mouse);
+   glutMotionFunc(motion);
+   if (fullScreenMode) {
+      glutFullScreen();             // Put into full screen
+   } else {
+      glutReshapeWindow(windowWidth, windowHeight); // Switch into windowed mode
+   }
+   initSim();
+   initGL();
+   glutMainLoop();               // Enter event-processing loop
+   return 0;
+}
